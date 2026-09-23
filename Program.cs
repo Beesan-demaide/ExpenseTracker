0719098d0a312f02f4ad5c0626afed715ad8a517
Beesan-demaide <beesan.demaide@gmail.com>
2026-09-23 20:56:47 +0300

    add gitignore and untrack build artifacts

--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source = expenses.db"));
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    if (!db.Categories.Any())
+    {
+        db.Categories.AddRange(
+            new Category { Name = "Food" },
+            new Category { Name = "Transport" },
+            new Category { Name = "Bills" },
+            new Category { Name = "Other" }
+        );
+        db.SaveChanges();
+    }
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
